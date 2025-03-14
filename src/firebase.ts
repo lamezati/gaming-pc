496bd5c434636a7f93a86f76d4dc8f8398ebfeeb
Leonel Mezatio <afrokab1@gmail.com>
2025-03-14 17:57:25 -0400

    Add Firebase configuration and service exports

---
+++ src/firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLTLN_RoZXRrH_AnAJ5uSA8NPcdxy3d0I",
+  authDomain: "gaming-pc-cebbe.firebaseapp.com",
+  projectId: "gaming-pc-cebbe",
+  storageBucket: "gaming-pc-cebbe.firebasestorage.app",
+  messagingSenderId: "670951072768",
+  appId: "1:670951072768:web:ece52403dd4992207b64e5",
+  measurementId: "G-4E7Q9X9N1N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, analytics, auth, db };

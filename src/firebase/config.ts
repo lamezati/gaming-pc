8c5f8a6b721a5a0244ae8de59c97619a47b148e7
Leonel Mezatio <afrokab1@gmail.com>
2025-03-15 16:35:59 -0400

    Update Firebase config to include Firestore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // Replace these with your actual Firebase configuration
@@ -17,4 +18,8 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Firestore
+export const db = getFirestore(app);
+
 export default app;
